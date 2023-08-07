5ac10780cdf7c37a38f176877768ac6ab00ac625
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-08-07 13:42:18 +0800

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Kelvin Ricafort on 8/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
